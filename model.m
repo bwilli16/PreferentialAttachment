f34bd810e2e03c2b29adb2676d9da36581236e08
bwilli16 <33357883+bwilli16@users.noreply.github.com>
2019-11-01 17:20:20 -0400

    Added p=P(second edge attached)
    Still contains self loops

--- model.m
+++ model.m
@@ -1,11 +1,19 @@
 %Specify parameters.
-timesteps = 10; % This is also the number of nodes.
+M=1050;
+N=1000;
+p=1-N/M;
+timesteps = M; % This is also the number of nodes.
 
 %Initialize adjacency matrix at time t = 0.
 A = zeros(1,1);
 
 %Add a node at each timestep.
 for t = 1:timesteps-1
+%    if rand()<p && t>10
+%        [x, y] = ind2sub(size(A), datasample(find(A==0),1) );
+%        A(x,y)=1;
+%        A(y,x)=1;
+%    end
     %Get a list of the degrees of each node (Sum the columns in A).
     deg = sum(A,1);
     %Get the age of each node.
@@ -14,7 +22,7 @@ for t = 1:timesteps-1
     %Compute age_weight based on some function. Weight ranges from 0 to 1.
     age_weight = arrayfun(@(x) 1/x, age);
     %Compute degree_weight based on some function. Weight ranges from 0 to 1.
-    deg_weight = arrayfun(@(x) x/max(deg),deg);
+    deg_weight = arrayfun(@(x) (x/max(deg))^2,deg);
     deg_weight(isnan(deg_weight)) = 1;
     
     %Compute total weight of each node. Weight ranges from 0 to 1.
@@ -30,13 +38,94 @@ for t = 1:timesteps-1
         end
     end
    
+    %
+
+    
     %Add the node to the adjacency matrix.
     A(n,size(A,2)+1) = 1;  
     A(size(A,1)+1,n) = 1;
+    
+    if rand()<p
+        nbors=find(A(:,n));
+        nbors=nbors(find(nbors~=A(size(A,1),n)));
+        
+        nbor_lin_idx=randsample(nbors,1);
+        [nbor_x n_copy]= ind2sub(size(A),nbor_lin_idx);
+        A(size(A,1),nbor_x) = 1;  
+        A(nbor_x,size(A,2)) = 1;
+    end
+end
+%%
+%MATLAB visualizations aren't that great, this is just for testing.
+deg = sum(A,1);
+age = [t+1:-1:1];
+G = graph(A);
+figure
+plot(G,'layout','force');
+
+%%
+adj2gephilab('toGephi',A);
+%%
+function EdgeL=adj2gephilab(filename,ADJ,parameters)
+    % Convert ana adjacency matrix of a graph to 2 spreadhseets csv files
+    % one for the edge table and the other for node table.
+    % The files _node.csv and _edge.csv have to be open 
+    %in Gephi via Data Laboratory.
+    % INPUTS:
+    %           filename: string for the prefix name of the two files .csv
+    %           ADJ: the adjacency matrix
+    %           parameters: vector as properties of the node to use as
+    %                              attributes of them.
+    % OUTPUTS:
+    %            two csv spreadsheet files: 
+    %                       filename_node.csv
+    %                       filename_edge.csv
+    %             EdgeL = it returns the edge list corresponing to the
+    %             adjacency matrix. (it can be saved to be open in Gephi too)
+    %
+    % The two files must be open in Gephi via the Data Laboratory
+    nodecsv=[filename,'_node.csv'];
+    edgecsv=[filename,'_edge.csv'];
+    n=size(ADJ,1); % square adjacency matrix
+    if nargin<3 
+        parameters=ones(n,1);% all nodes have the same attributes
+    end
+    ps=parameters;
+    % Node Table:
+    % header for node csv
+    fidN = fopen(nodecsv,'w','native','UTF-8'); % best format for gephi
+    fprintf(fidN,'%s\n','Id;Label;Attribute');
+    % 
+    for i=2:n+1,
+        fprintf(fidN,'%s\n',[ num2str(i-1) ';"Node ' num2str(i-1) '"'...
+            ';' num2str(ps(i-1))]);
+    end
+    fclose(fidN);
+    % Edge Table
+    EdgeL=conv_EdgeList(ADJ);
+    S=EdgeL(:,1); % sources
+    T=EdgeL(:,2); % targets
+    W = EdgeL(:,3); % weights
+    fidE = fopen(edgecsv,'w','native','UTF-8');
+    % header for edge csv
+    fprintf(fidE,'%s\n','Source;Target;Label;Weight');
+    for i=2:length(S)+1,
+          fprintf(fidN,'%s\n',[ num2str(S(i-1)) ';' num2str(T(i-1)) ';'...
+              '"Edge from ' num2str(S(i-1)) ' to ' num2str(T(i-1)) '"' ';'...
+              num2str(W(i-1))]);
+    end
+     fclose(fidE);
 end
 
-%MATLAB visualizations aren't that great, this is just for testing.
-deg = sum(A,1)
-age = [t+1:-1:1]
-G = graph(A);
-plot(G);
+%% Aux function
+function EdgeL=conv_EdgeList(adj)
+    % convert adj matrix to edge list
+    n=size(adj,1); % number of nodes
+    edges=find(adj>0); % indices of all edges
+    n_e=length(edges);
+    EdgeL=zeros(n_e,3);
+    for e=1:n_e
+      [i,j]=ind2sub([n,n],edges(e)); % node indices of edge e  
+      EdgeL(e,:)=[i j adj(i,j)];
+    end
+end
